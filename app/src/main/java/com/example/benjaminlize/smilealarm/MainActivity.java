110bbe69dd4d7ec3b34be2140b9d288fe8038abe
benjamin.lize <benjamin.lize94@gmail.com>
2016-05-11 15:59:35 +0100

    Main Activity and Edit Alarm UI v1

--- app/src/main/java/com/example/benjaminlize/smilealarm/MainActivity.java
+++ app/src/main/java/com/example/benjaminlize/smilealarm/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.benjaminlize.smilealarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView tv = (TextView)findViewById(R.id.alarm_time);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),EditAlarm.class));
+            }
+        });
     }
+
 }
